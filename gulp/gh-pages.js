3d1f8149f69a9db3d410f87713642f860c16f669
diffsky <matthew.hadley@gmail.com>
2014-09-01 22:18:21 -0700

    auto build gh-pages

--- gulp/gh-pages.js
+++ gulp/gh-pages.js
@@ -4,6 +4,7 @@ var marked = require('marked');
 var path = require('path');
 var fs = require('fs');
 var ejstpl = require('ejstpl');
+var mkdirp = require('mkdirp').sync;
 
 module.exports = function(gulp, conf) {
 
@@ -17,7 +18,7 @@ module.exports = function(gulp, conf) {
     }
   };
 
-  gulp.task('ghpages', function(cb) {
+  gulp.task('gh-pages', function(cb) {
 
     var readme = fs.readFileSync(path.join(process.cwd(), 'README.md'), 'utf8');
     data.js.appear = fs.readFileSync(path.join(process.cwd(), 'lib', 'appear.js'), 'utf8');
@@ -33,12 +34,15 @@ module.exports = function(gulp, conf) {
 
     var templates = ejstpl({cwd: path.join(process.cwd(), 'templates')});
 
-    var dest = path.join(process.cwd(), '..', 'appear-ghpages');
+    var dest = path.join(process.cwd(), '.gh-pages');
+    mkdirp(dest);
 
     fs.writeFileSync(path.join(dest, 'index.html'), templates.index(data));
     data.title = ' - simple example';
+    mkdirp(path.join(dest, 'examples', 'simple'));
     fs.writeFileSync(path.join(dest, 'examples', 'simple', 'index.html'), templates['examples/simple/index'](data));
     data.title = ' - lazyload example';
+    mkdirp(path.join(dest, 'examples', 'lazy'));
     fs.writeFileSync(path.join(dest, 'examples', 'lazy', 'index.html'), templates['examples/lazy/index'](data));
 
     cb();
